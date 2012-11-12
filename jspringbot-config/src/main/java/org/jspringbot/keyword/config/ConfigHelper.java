b9790934e400ea508010a53973fbd3f5ac5182e5
badong2210 <badong2210@yahoo.com>
2012-11-12 18:22:21 +0800

    added jspringbot-config library

---
+++ jspringbot-config/src/main/java/org/jspringbot/keyword/config/ConfigHelper.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2012. JSpringBot. All Rights Reserved.
+ *
+ * See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The JSpringBot licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jspringbot.keyword.config;
+
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class ConfigHelper {
+
+    protected Map<String, Resource> domains;
+
+    protected Map<String, Properties> domainProperties;
+
+    private String selectedDomain;
+
+    public void setDomains(Map<String, Resource> domains) {
+        this.domains = domains;
+    }
+
+    public void init() throws IOException {
+        domainProperties = new HashMap<String, Properties>(domains.size());
+
+        for (Map.Entry<String, Resource> entry : domains.entrySet()) {
+            Properties properties = new Properties();
+            properties.load(entry.getValue().getInputStream());
+
+            domainProperties.put(entry.getKey(), properties);
+        }
+    }
+
+    public void selectDomain(String selectedDomain) {
+        if (!domainProperties.containsKey(selectedDomain)) {
+            throw new IllegalArgumentException(String.format("Unsupported selected domain '%s'", selectedDomain));
+        }
+
+        this.selectedDomain = selectedDomain;
+    }
+
+    public String getStringProperty(String key) {
+        if (selectedDomain == null) {
+            throw new IllegalStateException("No domain selected");
+        }
+
+        Properties properties = domainProperties.get(selectedDomain);
+
+        if (!properties.containsKey(key)) {
+            throw new IllegalArgumentException(String.format("No property found for key '%s'", key));
+        }
+
+        return properties.getProperty(key);
+    }
+}
---
+++ jspringbot-config/src/test/java/org/jspringbot/keyword/config/ConfigHelperTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2012. JSpringBot. All Rights Reserved.
+ *
+ * See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The JSpringBot licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jspringbot.keyword.config;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Test for {@link org.jspringbot.keyword.config.ConfigHelper} class.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:sample-config.xml"})
+public class ConfigHelperTest {
+
+    @Autowired
+    protected ConfigHelper configHelper;
+
+    @Test
+    public void testSample() throws Exception {
+        configHelper.selectDomain("sample");
+
+        assertEquals("value", configHelper.getStringProperty("sample"));
+    }
+
+
+}
