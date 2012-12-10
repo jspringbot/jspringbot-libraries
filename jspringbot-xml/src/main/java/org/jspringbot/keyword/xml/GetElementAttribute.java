d4b044effca620cc89440d483ea99bee17ee0358
Alvin R. de Leon <alvinrdeleon@me.com>
2012-12-11 00:18:25 +0800

    added get element attribute

---
+++ jspringbot-xml/src/main/java/org/jspringbot/keyword/xml/GetElementAttribute.java
@@ -0,0 +1,39 @@
+package org.jspringbot.keyword.xml;
+
+import org.jspringbot.KeywordInfo;
+import org.jspringbot.syntax.HighlightRobotLogger;
+import org.jspringbot.syntax.HighlighterUtils;
+import org.springframework.stereotype.Component;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
+@Component
+@KeywordInfo(name = "Get Element Attribute", description = "Get Element Attribute", parameters = {"element", "attributeName"})
+public class GetElementAttribute  extends AbstractXMLKeyword {
+
+    public static final HighlightRobotLogger LOG = HighlightRobotLogger.getLogger(GetElementAttribute.class);
+
+    @Override
+    public Object execute(Object[] params) throws ParserConfigurationException, IOException, SAXException {
+        Element element = (Element) params[0];
+        String attributeName = String.valueOf(params[1]);
+
+        StringBuilder buf = new StringBuilder();
+        buf.append("<b>Element:</b>").append(HighlighterUtils.INSTANCE.highlightXML(XMLFormatter.prettyPrint(element)));
+
+        LOG.pureHtml(buf.toString());
+
+        String result = element.getAttribute(attributeName);
+
+        LOG.createAppender()
+                .appendBold("Get Element Attribute:")
+                .appendProperty("Attribute", attributeName)
+                .appendProperty("Result", result)
+                .log();
+
+        return result;
+    }
+}
