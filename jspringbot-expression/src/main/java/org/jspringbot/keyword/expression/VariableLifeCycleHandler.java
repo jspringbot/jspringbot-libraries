bb64da6170d105be870f5f57ddf0e965abeaf15c
Alvin R. de Leon <alvinrdeleon@me.com>
2013-01-10 08:00:42 +0800

    updated refreshing of expression test scope variables.

--- jspringbot-expression/src/main/java/org/jspringbot/keyword/expression/VariableLifeCycleHandler.java
+++ jspringbot-expression/src/main/java/org/jspringbot/keyword/expression/VariableLifeCycleHandler.java
@@ -1,26 +1,33 @@
 package org.jspringbot.keyword.expression;
 
+import org.jspringbot.MainContextHolder;
+import org.jspringbot.Visitor;
 import org.jspringbot.keyword.expression.plugin.DefaultVariableProviderImpl;
 import org.jspringbot.lifecycle.LifeCycleAdapter;
-import org.jspringbot.spring.ApplicationContextHolder;
+import org.jspringbot.spring.RobotScope;
+import org.jspringbot.spring.SpringRobotLibraryManager;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Map;
 
 public class VariableLifeCycleHandler extends LifeCycleAdapter {
-    private static DefaultVariableProviderImpl getDefaultVariable() {
-        if(ApplicationContextHolder.get() == null) {
-            return null;
-        }
-
-        return (DefaultVariableProviderImpl) ApplicationContextHolder.get().getBean("defaultVariableProvider");
-    }
+    private static final String DEFAULT_VARIABLE_PROVIDER_BEAN_NAME = "defaultVariableProvider";
 
     @Override
     public void startTest(String name, Map attributes) {
-        DefaultVariableProviderImpl variables = getDefaultVariable();
+        if(MainContextHolder.isEnabled()) {
+            SpringRobotLibraryManager manager = MainContextHolder.get().getBean(SpringRobotLibraryManager.class);
 
-        if(variables != null) {
-            variables.clear();
+            manager.visitActive(RobotScope.ALL, new Visitor<ClassPathXmlApplicationContext>() {
+                @Override
+                public void visit(ClassPathXmlApplicationContext context) {
+                    if(context.containsBean(DEFAULT_VARIABLE_PROVIDER_BEAN_NAME)) {
+                        DefaultVariableProviderImpl provider = (DefaultVariableProviderImpl) context.getBean(DEFAULT_VARIABLE_PROVIDER_BEAN_NAME);
+
+                        provider.clear();
+                    }
+                }
+            });
         }
     }
 }
