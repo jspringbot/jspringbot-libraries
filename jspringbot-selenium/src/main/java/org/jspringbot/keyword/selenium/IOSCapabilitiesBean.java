f045462309e3a1aeb7c7057f2587aede4185494f
Bernard Lago <bernardilago@gmail.com>
2015-06-23 20:11:27 +0800

    Initial commit for ios device - (safari) web automation testing.

---
+++ jspringbot-selenium/src/main/java/org/jspringbot/keyword/selenium/IOSCapabilitiesBean.java
@@ -0,0 +1,79 @@
+package org.jspringbot.keyword.selenium;
+
+import java.util.Iterator;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.openqa.selenium.Platform;
+import org.springframework.beans.factory.InitializingBean;
+import org.uiautomation.ios.IOSCapabilities;
+
+public class IOSCapabilitiesBean implements InitializingBean {
+    private static final Logger LOGGER = Logger.getLogger(IOSCapabilitiesBean.class);
+
+    private IOSCapabilities capabilities;
+    
+    private String browser = "Safari";
+
+    public IOSCapabilitiesBean(IOSCapabilities capabilities) {
+        this.capabilities = capabilities;
+    }
+    
+    public void setDeviceType(String device) {
+    	if(device.equalsIgnoreCase("iphone")){
+    		this.capabilities = IOSCapabilities.iphone(browser);
+    	}
+    }
+    
+    public void setDeviceId(String uuid) {
+    	if(!StringUtils.equalsIgnoreCase(uuid, "none")) {
+    		capabilities.setCapability(IOSCapabilities.UUID, uuid);
+    	}
+    }
+    
+    public void setSimulatorMode(boolean mode){
+    	capabilities.setCapability(IOSCapabilities.SIMULATOR, mode);
+    }
+   
+    public void setBrowserName(String browserName) {
+        if(!StringUtils.equalsIgnoreCase(browserName, "none")) {
+            capabilities.setBrowserName(browserName);
+        }
+    }
+
+    public void setVersion(String version){
+        if(!StringUtils.equalsIgnoreCase(version, "none")) {
+            capabilities.setVersion(version);
+        }
+    }
+
+    public void setPlatform(String platform){
+        if(!StringUtils.equalsIgnoreCase(platform, "none")) {
+            capabilities.setPlatform(Platform.valueOf(platform));
+        }
+    }
+
+
+    @SuppressWarnings("unchecked")
+    public void setCapabilities(String properties) throws JSONException {
+        if(!StringUtils.equalsIgnoreCase(properties, "none")) {
+            JSONObject obj = new JSONObject(properties);
+
+            Iterator<String> itr = obj.keys();
+            while(itr.hasNext()) {
+                String key = itr.next();
+                capabilities.setCapability(key, obj.getString(key));
+            }
+        }
+    }
+
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
